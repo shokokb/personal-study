7fdcd2dfb601bdb94306527c63b2d35b697d6cea
Shoko Kobayashi <shoko.kobayashi@gmail.com>
2019-09-13 10:40:09 +0900

    標準出力

---
+++ python-basic/basic.py
@@ -0,0 +1,26 @@
+# coding: UTF-8
+
+print "Hello World"
+
+print "====="
+
+print """Hello 
+World"""
+print \
+"Hello World"
+
+print "====="
+
+print 'a'
+
+print "====="
+
+# NoneType
+print None
+
+print "====="
+
+# 末尾にコロンを打つ
+for i in range(10) :	# 0 ... 9
+	print("Hello World %d" % i)
+
