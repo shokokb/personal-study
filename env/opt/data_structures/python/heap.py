4a03fa8a7cf0443334dfa954bba6fd0cd4740d33
shokokb <shoko.kobayashi@gmail.com>
2019-10-06 15:45:43 +0900

    Heapソート

--- env/opt/data_structures/python/heap.py
+++ env/opt/data_structures/python/heap.py
@@ -1,104 +1,61 @@
 # 頭頂部に最小値が来るヒープ
 class Heap:
     def __init__(self):
-        self.data = []
+        self.heap = []
 
-    # ながさ
-    def len(self):
-        return len(self.data)
+    def add(self, v):
+        self.heap.append(v)
+        n = len(self.heap) - 1
+        p = (n - 1) // 2
+        while n > 0:
+            # 親が自分より小さければ抜ける
+            if self.heap[p] <= self.heap[n]:
+                break
+            else:
+                self.heap[n], self.heap[p]  = self.heap[p], self.heap[n]
+            n = p  # p
 
-    # 深さ
-    def depth(self):
-        i = 1
-        depth = 0
-        while (i <= len(self.data)):
-           i = i * 2
-           depth += 1
-        return depth
-    
-    # 親ノードの添字
-    def p(c):
-        return (c - 1) // 2
-    
-    # 左側の子ノードの添字
-    def cl(p):
-        return 2 * p + 1
-    
-    # 右側の子ノードの添字
-    def cr(p):
-        return 2 * (p + 1)
-    
-    # 追加
-    def heapup(self, val):
-        self.data.append(int(val))
-        n = self.len() - 1
-        while n > 1:
-            p = Heap.p(n)
-            if (self.data[p] > self.data[n]):
-                self.data[p], self.data[n] = self.data[n], self.data[p]
+    def remove(self):
+        if len(self.heap) == 1:
+            return self.heap[0]
+        min = self.heap[0]
+        last = self.heap.pop()
+        self.heap[0] = last
+        n = 0
+        while n < len(self.heap):
+            cn = 2 * n + 1 # 左側の子
+            cr = 2 * n + 2  # 右側の子
+            if cr < len(self.heap):
+                c = self.heap[cn]  
+                r = self.heap[cr]
+                if r < c:       # 右の子の方が小さい場合
+                    cn = cr
+                    c = r
+            elif cn < len(self.heap):
+                c = self.heap[cn]  
             else:
                 break
-            n = n // 2
-
-    def heapdown(self):
-        ret = self.data[0]
-        self.data[0] = self.data.pop()
-        p = 0
-        # 現在のノードがRangeOverしない限り
-        while (p < self.len() - 1):
-            cl = Heap.cl(p)
-            cr = Heap.cr(p)
-            # 右側の子ノードがある場合
-            if cr < self.len():
-                if self.data[cr] < self.data[cl]:
-                    c = cr
-                else:
-                    c = cl
-            # 左側の子ノードがある場合
-            elif cl < self.len():
-                c = cl
-            # そのほかの場合
+            if c < last:
+                self.heap[n], self.heap[cn] = self.heap[cn], self.heap[n]
             else:
-                return
+                break
+            n = cn
+        return min
 
-            if c < self.data[p]:
-                self.data[c], self.data[p] = self.data[p], self.data[c]
-                p = c
-            else:
-                return
-        return
+    def sort(list):
+        h = Heap()
+        for n in list:
+            h.add(n)
+        print(h.heap)
 
-    # ヒープソート
-    def sort(self):
-        sorted = []
-        while self.len() > 1:
-            sorted.append(self.data[0])
-            self.heapdown()
-        if self.len() == 1:
-            sorted.append(self.data[0])
-        return sorted
+        ans = []
+        for n in list:
+            ans.append(h.remove())
+        return ans
 
-    # 標準出力
-    def print(self):
-        for n in self.data:
-            print(str(n) + " ", end="")
-        print("")
-    
             
 if __name__ == "__main__":
-    h = Heap()
-    h.heapup(1)
-    h.heapup(3)
-    h.heapup(6)
-    h.heapup(4)
-    h.heapup(8)
-    h.heapup(7)
-    h.heapup(5)
-    # h.print()
-    print(h.sort())
-    # print(Heap.p(1) == 0)
-    # print(Heap.p(2) == 0)
-    # print(Heap.p(9) == 4)
-    # print(Heap.p(12) == 5)
-    # print(Heap.cl(3) == 7)
-    # print(Heap.cl(3) == 8)
+    ans = Heap.sort([1, 3, 6, 4, 8, 7, 5])
+    print(ans)
+    ans = Heap.sort([2, 7, 11, 15])
+    print(ans)
