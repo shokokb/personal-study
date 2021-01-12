c3721ca53c50a207b28fc6f13adbb65b6cdb7678
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-12 22:58:58 +0900

    2. Add Two Numbers Success Details Runtime: 64 ms, faster than 89.50% of Python3 online submissions for Add Two Numbers. Memory Usage: 14.5 MB, less than 11.20% of Python3 online submissions for Add Two Numbers.

--- env/opt/leetcode/0002_Add_Two_Numbers.py
+++ env/opt/leetcode/0002_Add_Two_Numbers.py
@@ -5,9 +5,15 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        n = 0
-        result = 0
-        while l1 or l2:
+        n1, n2 = l1.val, l2.val
+        n = (n1 + n2) % 10
+        carry = (n1 + n2) // 10
+        head = node = ListNode(n)
+        # print(n1, n2, n, carry)
+        l1 = l1.next
+        l2 = l2.next
+        
+        while l1 or l2 or carry:
             n1, n2 = 0, 0
             if l1:
                 n1 = l1.val
@@ -15,14 +21,12 @@ class Solution:
             if l2:
                 n2 = l2.val
                 l2 = l2.next
-            result += (n1 + n2) * 10 ** n
-            n += 1
-        
-        head = node = ListNode(result % 10)
-        result //= 10
-        while result > 0:
-            node.next = ListNode(result % 10)
-            result //= 10 
+            
+            n = (n1 + n2 + carry) % 10
+            carry = (n1 + n2 + carry) // 10
+            # print(n1, n2, n, carry)
+
+            node.next = ListNode(n)
             node = node.next
+            
         return head
-        
