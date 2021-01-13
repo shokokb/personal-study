e8d3c9e7d07f2924128eb5e13a932341027ed967
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-13 18:57:53 +0900

    1266. Minimum Time Visiting All Points Success Details Runtime: 60 ms, faster than 58.64% of Python3 online submissions for Minimum Time Visiting All Points. Memory Usage: 14.4 MB, less than 6.38% of Python3 online submissions for Minimum Time Visiting All Points.

---
+++ env/opt/leetcode/1266_Minimum_Time_Visiting_All_Points.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        # ret = 0
+        # for i in range(len(points)-1):
+        #     ret += max(abs(points[i][0]-points[i+1][0]), abs(points[i][1]-points[i+1][1]))
+        # return ret
+        return sum([max(abs(points[i][0]-points[i+1][0]), abs(points[i][1]-points[i+1][1])) for i in range(len(points)-1)])
