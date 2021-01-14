2aa697c8232a427deb2ab5cd87a6a5dfe1c940fa
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-14 19:15:47 +0900

    Array

--- env/opt/data_structures/array.py
+++ env/opt/data_structures/array.py
@@ -1,53 +1,7 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-
-class Array:
-    def __init__(self):
-        self.size = 0
-        self.array = []
-
-    # Random Access(O(1))
-    def data(idx):
-        return self.array[idx].data
-
-    def append(self, data):
-        self.insert(self.size, data)
-
-    # insertion(O(n))
-    def insert(self, idx, data):
-        if self.size == 0:
-            self.array.append(Node(data)) 
-        else:
-            self.array.append(None)
-            # 後ろに一つずつずらす
-            i = self.size
-            for i in reversed(range(idx, self.size)):
-                self.array[i + 1] = self.array[i]
-            self.array[idx] = Node(data)
-        self.size += 1
-
-    def remove(self, idx):
-        for i in reversed(range(idx, self.size - 1)):
-            self.array[i] = self.array[i + 1]
-        self.size -= 1
-
-    def print_all(self):
-        for i in range(self.size):
-            print(str(self.array[i].data) + " ", end = "")
-        print()
+# coding: UTF-8
 
 if __name__ == "__main__":
-    ary = Array()
-    print("size=" + str(ary.size))
-    ary.append(100)
-    ary.append(300)
-    ary.append(200)
-    ary.print_all()
-    print("size=" + str(ary.size))
-    ary.insert(1, 400)
-    ary.print_all()
-    ary.insert(0, 500)
-    ary.print_all()
-    ary.remove(3)
-    ary.print_all()
+    print("list or dequeで実装できる")
+
+    arr = [i for i in range(5)]
+    print(arr)
