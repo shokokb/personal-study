5b813018000235d9a462a15fa10fb2f53e205087
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-14 18:25:36 +0900

    Queue

--- env/opt/data_structures/queue.py
+++ env/opt/data_structures/queue.py
@@ -1,30 +1,12 @@
-class Queue:
-    def __init__(self):
-        self.items = []
-    def is_empty(self):
-        return len(self.items) == 0
-    def enqueue(self, item):
-        self.items.insert(0, item)
-    def dequeue(self):
-        return self.items.pop()
-    def size(self):
-        return len(self.items)
+# coding: UTF-8
 
 if __name__ == "__main__":
-    print("=== Queue(Class) ===")
-    queueObj = Queue()
-    # print(queueObj.is_empty())
-    queueObj.enqueue("Hello")
-    queueObj.enqueue("World")
-    queueObj.enqueue("Mr")
-    queueObj.enqueue("Japan")
-    dequeued = queueObj.dequeue()
-    print(dequeued)
-    dequeued = queueObj.dequeue()
-    print(dequeued)
-    dequeued = queueObj.dequeue()
-    print(dequeued)
-    dequeued = queueObj.dequeue()
-    print(dequeued)
-    print("\n")
-    print("\n")
+    print("list or dequeで実装できる")
+
+    l = []
+    l.append(3)
+    l.append(4)
+    l.append(5)
+    print(l.pop(0))
+    print(l.pop(0))
+    print(l.pop(0))
