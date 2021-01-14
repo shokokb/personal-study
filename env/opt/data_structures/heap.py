ed72fcd872f202f4a3d8ffdda063cd8182648e96
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-14 19:33:20 +0900

    Heap

--- env/opt/data_structures/heap.py
+++ env/opt/data_structures/heap.py
@@ -1,61 +1,83 @@
-# 頭頂部に最小値が来るヒープ
+# coding: UTF-8
+
+from collections import deque 
+
 class Heap:
+    @classmethod
     def __init__(self):
-        self.heap = []
+        self.l = []
 
-    def add(self, v):
-        self.heap.append(v)
-        n = len(self.heap) - 1
-        p = (n - 1) // 2
-        while n > 0:
-            # 親が自分より小さければ抜ける
-            if self.heap[p] <= self.heap[n]:
-                break
-            else:
-                self.heap[n], self.heap[p]  = self.heap[p], self.heap[n]
-            n = p  # p
+    @classmethod
+    def size(self):
+        return len(self.l)
 
+    @classmethod
+    def insert (self, val):
+        self.l.append(val)
+        i = self.size()
+        while i > 1:
+            pi = i // 2
+            if self.l[pi-1] > self.l[i-1]:
+                self.l[pi-1], self.l[i-1] = self.l[i-1], self.l[pi-1]
+            i = pi
+        
+    @classmethod
     def remove(self):
-        if len(self.heap) == 1:
-            return self.heap[0]
-        min = self.heap[0]
-        last = self.heap.pop()
-        self.heap[0] = last
-        n = 0
-        while n < len(self.heap):
-            cn = 2 * n + 1 # 左側の子
-            cr = 2 * n + 2  # 右側の子
-            if cr < len(self.heap):
-                c = self.heap[cn]  
-                r = self.heap[cr]
-                if r < c:       # 右の子の方が小さい場合
-                    cn = cr
-                    c = r
-            elif cn < len(self.heap):
-                c = self.heap[cn]  
-            else:
+        
+        if self.size() == 0:
+            return
+
+        if self.size() == 1:
+            first = self.l.pop(0)
+            return first
+
+        first = self.l.pop(0)
+        last = self.l.pop()
+        self.l.insert(0, last)
+        pi = 1
+        li = 2 * pi
+        ri = 2 * pi + 1
+        i = ri if ri <= self.size() and self.l[ri-1] < self.l[li-1] else li
+        while i <= self.size():
+            if self.l[pi-1] > self.l[i-1]:
+                self.l[pi-1], self.l[i-1] = self.l[i-1], self.l[pi-1]
+                pi = i
+                li = 2 * pi
+                ri = 2 * pi + 1
+                i = ri if ri <= self.size() and self.l[ri-1] < self.l[li-1] else li
+            else: 
                 break
-            if c < last:
-                self.heap[n], self.heap[cn] = self.heap[cn], self.heap[n]
-            else:
-                break
-            n = cn
-        return min
+        print(first, self.l)
+        return first
+	
+    @classmethod
+    def sort(self):
+        ret = []
+        while self.size() > 0:
+            n = self.remove()
+            ret.append(n)
+        return ret
 
-    def sort(list):
-        h = Heap()
-        for n in list:
-            h.add(n)
-        print(h.heap)
+    @classmethod
+    def display (self):
+        print(self.l)
 
-        ans = []
-        for n in list:
-            ans.append(h.remove())
-        return ans
 
-            
 if __name__ == "__main__":
-    ans = Heap.sort([1, 3, 6, 4, 8, 7, 5])
-    print(ans)
-    ans = Heap.sort([2, 7, 11, 15])
-    print(ans)
+    h = Heap()
+
+    h.insert(1)
+    h.insert(3)
+    h.insert(6)
+    h.insert(4)
+    h.insert(8)
+    h.insert(7)
+    h.insert(5)
+    h.insert(2)
+    
+    h.display()
+
+    n = h.remove()
+    print("remove=", n)
+    h.display()
+
