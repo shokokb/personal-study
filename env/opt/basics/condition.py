fa67c10eee339762c0666dd594b92b4d87fccd45
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-14 17:57:46 +0900

    フォルダ整理

--- env/opt/basics/condition.py
+++ env/opt/basics/condition.py
@@ -1,5 +1,7 @@
 # coding: UTF-8
 
+if __name__ == "__main__":
+
 home = "JP"
 if home == "USA":
 	print "Hello, USA"
---
+++ env/opt/data_structures/deque.py
@@ -0,0 +1,9 @@
+# coding: UTF-8
+
+from collections import deque
+
+if __name__ == "__main__":
+
+    print("deque")
+
+    q = deque()
