b18416914e154704d1d50dd71b9b47364a1ce274
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-14 18:25:26 +0900

    Stack

--- env/opt/data_structures/stack.py
+++ env/opt/data_structures/stack.py
@@ -1,27 +1,12 @@
-# Stack(FIFO)
-class Stack:
-    def __init__(self):
-        self.items = []
-    def is_empty(self):
-        return len(self.items) == 0
-    def push(self, item):
-        self.items.append(item)
-    def pop(self):
-        return self.items.pop()
-    def peek(self):
-        last = len(self.items)-1
-        return self.items[last]
-    def size(self):
-        return len(self.items)
+# coding: UTF-8
 
 if __name__ == "__main__":
-    print("=== Stack(Class) ===")
-    text = "Hello"
-    stackObj = Stack()
-    # print(stackObj.is_empty())
-    for c in text:
-        stackObj.push(c)
-    reversed_text = ""
-    for i in range(stackObj.size()):
-        reversed_text += stackObj.pop()
-    print(reversed_text)
+    print("list or dequeで実装できる")
+
+    l = []
+    l.append(3)
+    l.append(4)
+    l.append(5)
+    print(l.pop())
+    print(l.pop())
+    print(l.pop())
