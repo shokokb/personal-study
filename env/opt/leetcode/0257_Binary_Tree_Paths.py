8ba418e785381c7e17a2b55485629d3ebafe475a
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-16 10:12:58 +0900

    257. Binary Tree Paths Runtime: 24 ms, faster than 97.77% of Python3 online submissions for Binary Tree Paths. Memory Usage: 14.3 MB, less than 58.59% of Python3 online submissions for Binary Tree Paths.

---
+++ env/opt/leetcode/0257_Binary_Tree_Paths.py
@@ -0,0 +1,21 @@
+class Solution:
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if not root:
+            return []
+        
+        paths = []
+        stack = [(root, str(root.val))]
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right:
+                paths.append(path)
+            if node.left:
+                stack.append((node.left, path + '->' + str(node.left.val)))
+            if node.right:
+                stack.append((node.right, path + '->' + str(node.right.val)))
+        
+        return paths
