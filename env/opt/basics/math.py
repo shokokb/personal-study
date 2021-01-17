556fa73e43ab6cf1e44d9eccc63f9f193e31b7f1
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-17 16:17:40 +0900

    math

---
+++ env/opt/basics/math.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# Your code here!
+print(int(2.5))
+print(float(1))
+print(round(3.5))
+d, e = divmod(5, 3)
+
+print("divmod", d, e)
+print("abs", abs(2 - 5))
+print("min", min(2, 3))
+print("max", max(5, 2))
+
+print("bool", bool(0))
+print("bool", bool("false"))
+print("bool", bool(""))
+
+print("bin", bin(5))
+print("oct", oct(8))
+print("hex", hex(15))
+
+print("complex", complex(2, 3))
+
+print("pow", 2 ** 10)
+print("pow", pow(2, 10))
