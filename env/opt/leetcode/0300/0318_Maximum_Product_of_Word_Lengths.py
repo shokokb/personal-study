78b7b1b643af9b04df69ef7a7754115b4ff9fb22
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-17 15:29:26 +0900

    318. Maximum Product of Word Lengths Bitmask+Hash Success Details Runtime: 256 ms, faster than 87.76% of Python3 online submissions for Maximum Product of Word Lengths. Memory Usage: 14.4 MB, less than 94.94% of Python3 online submissions for Maximum Product of Word Lengths.

--- env/opt/leetcode/0300/0318_Maximum_Product_of_Word_Lengths.py
+++ env/opt/leetcode/0300/0318_Maximum_Product_of_Word_Lengths.py
@@ -1,21 +1,21 @@
+from collections import defaultdict
 class Solution:
-    # TimeComplexty O(n^2)
     def maxProduct(self, words: List[str]) -> int:
-        def get_bit_mask (s) -> int:
-            bit_number = lambda ch: ord(ch) - ord('a')
-            bit_mask = 0
-            for c in s:
-                bit_mask |= 1 << bit_number(c) 
-            return bit_mask
-                        
-        n = len(words)
+        hashmap = defaultdict(int)
+        bit_number = lambda ch : ord(ch) - ord('a')
+        
+        for word in words:
+            bitmask = 0
+            for ch in word:
+                # add bit number bit_number in bitmask
+                bitmask |= 1 << bit_number(ch)
+            # there could be different words with the same bitmask
+            # ex. ab and aabb
+            hashmap[bitmask] = max(hashmap[bitmask], len(word))
+        
         max_prod = 0
-
-        # O(n)
-        bit_masks = [get_bit_mask(words[i]) for i in range(n)]
-        # O(n^2)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if bit_masks[i] & bit_masks[j] == 0:
-                    max_prod = max(max_prod, len(words[i]) * len(words[j]))
+        for x in hashmap:
+            for y in hashmap:
+                if x & y == 0:
+                    max_prod = max(max_prod, hashmap[x] * hashmap[y])
         return max_prod
