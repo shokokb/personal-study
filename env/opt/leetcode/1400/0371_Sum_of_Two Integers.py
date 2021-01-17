8b1f10e06c97308066ca289310d7285cd92da7c7
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-17 13:54:37 +0900

    371. Sum of Two Integers Runtime: 52 ms, faster than 5.32% of Python3 online submissions for Sum of Two Integers. Memory Usage: 14.2 MB, less than 49.97% of Python3 online submissions for Sum of Two Integers.

---
+++ env/opt/leetcode/1400/0371_Sum_of_Two Integers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        x, y = abs(a), abs(b)
+        # ensure that abs(a) >= abs(b)
+        if x < y:
+            return self.getSum(b, a)
+        
+        # abs(a) >= abs(b) --> 
+        # a determines the sign
+        sign = 1 if a > 0 else -1
+        
+        if a * b >= 0:
+            # sum of two positive integers x + y
+            # where x > y
+            x = sum([x, y])
+        else:
+            # difference of two integers x - y
+            # where x > y
+            x = sum([x, -y])
+        return x * sign
