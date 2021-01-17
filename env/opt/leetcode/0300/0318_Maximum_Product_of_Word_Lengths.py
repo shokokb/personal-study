a5897c085fc25eb304c340cdfdc82a008ffe53c8
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-17 15:07:27 +0900

    318. Maximum Product of Word Lengths Native Solution Time Limit Exceeded

--- env/opt/leetcode/0300/0318_Maximum_Product_of_Word_Lengths.py
+++ env/opt/leetcode/0300/0318_Maximum_Product_of_Word_Lengths.py
@@ -1,16 +1,21 @@
 class Solution:
+    # TimeComplexty O(n^2)
     def maxProduct(self, words: List[str]) -> int:
-        def no_common_letters(s1, s2) -> bool:
-            for c1 in s1:
-                for c2 in s2:
-                    if c1 == c2:
-                        return False
-            return True
-            
+        def get_bit_mask (s) -> int:
+            bit_number = lambda ch: ord(ch) - ord('a')
+            bit_mask = 0
+            for c in s:
+                bit_mask |= 1 << bit_number(c) 
+            return bit_mask
+                        
         n = len(words)
         max_prod = 0
+
+        # O(n)
+        bit_masks = [get_bit_mask(words[i]) for i in range(n)]
+        # O(n^2)
         for i in range(n):
             for j in range(i + 1, n):
-                if no_common_letters(words[i], words[j]):
+                if bit_masks[i] & bit_masks[j] == 0:
                     max_prod = max(max_prod, len(words[i]) * len(words[j]))
         return max_prod
