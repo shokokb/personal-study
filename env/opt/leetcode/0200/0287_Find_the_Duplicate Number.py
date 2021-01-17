c34a72802627f84dad7d0bf19f1100bcadf4d511
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-17 17:10:34 +0900

    287. Find the Duplicate Number Success Details Runtime: 56 ms, faster than 97.36% of Python3 online submissions for Find the Duplicate Number. Memory Usage: 18.3 MB, less than 16.57% of Python3 online submissions for Find the Duplicate Number.

--- env/opt/leetcode/0200/0287_Find_the_Duplicate Number.py
+++ env/opt/leetcode/0200/0287_Find_the_Duplicate Number.py
@@ -1,6 +1,11 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        c = collections.Counter(nums)
-        for n, cnt in c.items():
-            if cnt > 1:
+        # c = collections.Counter(nums)
+        # for n, cnt in c.items():
+        #     if cnt > 1:
+        #         return n
+        seen = set()
+        for n in nums:
+            if n in seen:
                 return n
+            seen.add(n)
