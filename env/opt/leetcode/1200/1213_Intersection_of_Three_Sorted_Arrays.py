6773cef83b4270315e5ee7c718bc361e58d4d6d4
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-18 20:20:26 +0900

    1213. Intersection of Three Sorted Arrays Success Details Runtime: 120 ms, faster than 19.39% of Python3 online submissions for Intersection of Three Sorted Arrays. Memory Usage: 14.5 MB, less than 87.38% of Python3 online submissions for Intersection of Three Sorted Arrays.

--- env/opt/leetcode/1200/1213_Intersection_of_Three_Sorted_Arrays.py
+++ env/opt/leetcode/1200/1213_Intersection_of_Three_Sorted_Arrays.py
@@ -1,6 +1,21 @@
 class Solution:
-    def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:  
-        c = collections.Counter(arr1 + arr2 + arr3)
-        ret = list(filter(lambda k: c[k] == 3, c.keys()))
+    def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
+        ret = []
+        p1, p2, p3 = 0, 0, 0
+        while p1 < len(arr1) and p2 < len(arr2) and p3 < len(arr3):
+            if arr1[p1] == arr2[p2] == arr3[p3]:
+                ret.append(arr1[p1])
+                p1 += 1
+                p2 += 1
+                p3 += 1
+                continue
+            
+            if arr1[p1] < arr2[p2]:
+                p1 += 1
+            elif arr2[p2] < arr3[p3]:
+                p2 += 1
+            else:
+                p3 += 1
+            
         return ret
         
