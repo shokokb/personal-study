41a8ec8ca565058cdcd1d4b60a57f38357abe5a7
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-23 18:40:44 +0900

    797. All Paths From Source to Target Success Details Runtime: 100 ms, faster than 71.44% of Python3 online submissions for All Paths From Source to Target. Memory Usage: 16.2 MB, less than 6.06% of Python3 online submissions for All Paths From Source to Target.

--- env/opt/leetcode/0700/0797_All_Paths_From_Source_to_Target.py
+++ env/opt/leetcode/0700/0797_All_Paths_From_Source_to_Target.py
@@ -1,10 +1,15 @@
 class Solution:
+    def __init__(self):
+        self._paths = defaultdict(lambda: None)
+        
     def paths(self, start:int, graph:List[List[int]]) -> List[List[int]]:
         ret = []
         if start == len(graph) - 1:
             return [[start]]
         for np in graph[start]:
-            next_paths = self.paths(np, graph)
+            next_paths = self._paths[np]
+            if self._paths[np] is None:
+                self._paths[np] = next_paths = self.paths(np, graph)                
             for next_path in next_paths:
                 path = [start]
                 path.extend(next_path)
