d4197c43e38902725ff005c29fc2b5e9368ebfb9
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-23 13:54:55 +0900

    defaultdict

--- env/opt/basics/data/dict.py
+++ env/opt/basics/data/dict.py
@@ -1,11 +1,15 @@
 # coding: utf-8
-# Your code here!
-hash = {}
-hash["apple"] = 120
-hash["banana"] = 100
-print(hash["banana"])
+
+from collections import defaultdict
+
+print("=== dict ===")
+
+hash1 = {}
+hash1["apple"] = 120
+hash1["banana"] = 100
+print(hash1["banana"])
 try:
-    print(hash["grape"])
+    print(hash1["grape"])
 except KeyError:
     print("key error exception")
 except:
@@ -13,12 +17,16 @@ except:
 finally:
     print("finally")
     
-hash1 = {"apple" : 120, "banana": 100}
-print("hash1", hash1["apple"])
-
-hash2 = dict(apple=120, banana=100)
+hash2 = {"apple" : 120, "banana": 100}
 print("hash2", hash2["apple"])
 
-hash3 = dict(grape=200, pear=300)
-hash4 = dict(**hash2,**hash3)
-print(hash4)
+hash3 = dict(apple=120, banana=100)
+hash4 = dict(grape=200, pear=300)
+print("hash3", hash3)
+print("hash4", hash4)
+hash5 = dict(**hash3,**hash4)
+print("hash5(連結)", hash5)
+
+print("=== defaultdict ===")
+hash6 = defaultdict(lambda: None)
+print(hash6["grape"])
