eece19de11b6b6b3fd93d4f315bf6cb5d81c0354
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-24 14:20:12 +0900

    891. Sum of Subsequence Widths Success Details Runtime: 3388 ms, faster than 13.04% of Python3 online submissions for Sum of Subsequence Widths. Memory Usage: 17 MB, less than 22.61% of Python3 online submissions for Sum of Subsequence Widths.

--- env/opt/leetcode/0800/0891_Sum_of_Subsequence_Widths.py
+++ env/opt/leetcode/0800/0891_Sum_of_Subsequence_Widths.py
@@ -1,8 +1,8 @@
 class Solution:
     def sumSubseqWidths(self, A: List[int]) -> int:
-        sumv = 0
-        for i in range(2, len(A) + 1):
-            items = combinations(A, i)
-            for item in items:
-                sumv += max(item) - min(item)
-        return sumv
+        ret = 0
+        A.sort()
+        n = len(A)
+        for i in range(n):
+            ret += (2**i - 2 **(n - i - 1)) * A[i]
+        return ret % (10**9 + 7)
