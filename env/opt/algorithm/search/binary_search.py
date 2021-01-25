a160b60c70905e16f0aea302732c9146cf194575
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-25 21:00:57 +0900

    Binary Search

--- env/opt/algorithm/search/binary_search.py
+++ env/opt/algorithm/search/binary_search.py
@@ -1,21 +1,28 @@
 # coding: UTF-8
+from typing import List
 
-def binary_search(list, item):
-	low = 0
-	high = len(list) - 1
+# Time Complexy:O(logN) ただし、SortのTime Complexity:O(NlogN)
+# Space Complexity:O(1) 
+def search(l:List[int], t:int):
+	if len(l) == 0:
+		return None
 
-	while low <= high:
-		mid = (low + high) // 2
-		guess = list[mid]
-		if guess == item:
-			return mid
-		elif guess > item:
-			high = mid - 1
-		else:
-			low = mid + 1
+	l.sort()
+	pivot = len(l) // 2
+	if l[pivot] == t:
+		retur
+		# Time Complexy:O(logN)n t
+	if search(l[0:pivot], t) is not None:
+		retur
+		# Time Complexy:O(logN)n t
+	if search(l[pivot+1:], t) is not None:
+		return t
+	
 	return None
 
 if __name__ == "__main__":
-	my_list = [1, 3, 5, 7, 9]
-	print(binary_search(my_list, 3))
-	print(binary_search(my_list, -1))
+	my_list = [1
+	# Time Complexy:O(logN), 3, 5, 7, 9]
+	print(search
+	# Time Complexy:O(logN)(my_list, 3))
+	print(search(my_list, -1))
