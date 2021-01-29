fe23960b23be4864e5b7f3f7e48a97d97f338cfc
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-29 23:46:03 +0900

    個数制限付き部分和問題

---
+++ env/opt/algorithm/dynamic_programing/個数制限付部分和問題.py
@@ -0,0 +1,15 @@
+# coding: UTF-8
+from typing import List
+
+
+def solve(n:int, a:List[int], m:List[int], k:int) -> bool:
+    return True
+
+
+if __name__ == "__main__":
+    n = 3
+    a = [3, 4, 5]
+    m = [3, 2, 2]
+    K = 17
+    ans = solve(n, a, m, K)
+    print(ans)
