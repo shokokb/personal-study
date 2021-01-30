55478a7e44e932d9b1051d414cf9156116651e89
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-30 23:47:34 +0900

    283. Move Zeroes Success Details Runtime: 60 ms, faster than 31.66% of Python3 online submissions for Move Zeroes. Memory Usage: 15.3 MB, less than 64.49% of Python3 online submissions for Move Zeroes.

---
+++ env/opt/leetcode/0200/0283_Move_Zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] == 0:
+                n = nums.pop(i)
+                nums.append(n)
