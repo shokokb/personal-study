3197573f35efaf5776679bc4c715c46436c913b5
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-30 21:40:25 +0900

    387. First Unique Character in a String Success Details Runtime: 92 ms, faster than 87.86% of Python3 online submissions for First Unique Character in a String. Memory Usage: 14.6 MB, less than 16.96% of Python3 online submissions for First Unique Character in a String.

---
+++ env/opt/leetcode/0300/0387_First_Unique_Character_in_a_String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        cnt = collections.Counter(s)
+        for i, c in enumerate(s):
+            if cnt[c] == 1:
+                return i
+        return -1
+        
