554010519e6886228d3640879c1aeaf57bb0a3f9
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-01-30 14:15:55 +0900

    1551. Minimum Operations to Make Array Equal Success Details Runtime: 28 ms, faster than 92.23% of Python3 online submissions for Minimum Operations to Make Array Equal. Memory Usage: 14.2 MB, less than 51.77% of Python3 online submissions for Minimum Operations to Make Array Equal.

--- env/opt/leetcode/1500/1551_Minimum_Operations_to_Make_Array_Equal.py
+++ env/opt/leetcode/1500/1551_Minimum_Operations_to_Make_Array_Equal.py
@@ -1,12 +1,3 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        arr = [2 * i + 1 for i in range(n)]
-        # print(arr)
-        center = (arr[0] + arr[-1]) // 2
-        # print("center", center)
-        pivot = (n - 1) // 2
-        # print("pivot", pivot)
-        cnt = 0
-        for i in range(pivot + 1):
-            cnt += center - arr[i]
-        return cnt
+        return (n ** 2 - 1) // 4 if n % 2 == 1 else  (n ** 2) // 4
