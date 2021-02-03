d2ca3554aeb18e043450fed66085b527d1090187
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-03 18:25:04 +0900

    349. Intersection of Two Arrays Success Details Runtime: 40 ms, faster than 92.86% of Python3 online submissions for Intersection of Two Arrays. Memory Usage: 14.6 MB, less than 14.90% of Python3 online submissions for Intersection of Two Arrays.

--- env/opt/leetcode/0300/349_Intersection_of_Two_Arrays.py
+++ env/opt/leetcode/0300/349_Intersection_of_Two_Arrays.py
@@ -1,10 +1,4 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        cnt1 = collections.Counter(nums1)
-        cnt2 = collections.Counter(nums2)
-        ans = []
-        for k in cnt1.keys():
-            if cnt2[k] > 0:
-                ans.extend([k])
-        return ans
+        return set(nums1) & set(nums2)
         
