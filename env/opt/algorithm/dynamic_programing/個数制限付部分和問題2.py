06a6c37dab77b5f31e2b811f2ad438d1207ce14d
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-03 21:23:16 +0900

    個数制限部分和問題2

--- env/opt/algorithm/dynamic_programing/個数制限付部分和問題2.py
+++ env/opt/algorithm/dynamic_programing/個数制限付部分和問題2.py
@@ -2,15 +2,17 @@
 from typing import List
 
 def answer(n:int, a:List[int], m:List[int], k:int):
-    dp = [[False for _ in range(k + 1)] for _ in range(n + 1)]
-    dp[0][0] = True
+    dp = [-1 for _ in range(k + 1)]
+    dp[0] = 0
     for i in range(n):
         for j in range(K + 1):
-            for k in range(m[i] + 1):
-                if k * a[i] > j:
-                    break
-                dp[i + 1][j] |= dp[i][j - k * a[i]]
-    return dp[n][K]
+            if dp[j] >= 0:
+                dp[j] = m[i]
+            elif j < a[i] or dp[j - a[i]] <= 0:
+                dp[j] = -1
+            else:
+                dp[j] = dp[j - a[i]] - 1
+    return dp[K] >= 0
 
 if __name__ == "__main__":
     n = 3
