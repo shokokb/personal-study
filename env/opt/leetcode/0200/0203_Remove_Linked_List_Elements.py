22d1ac334e44d6999841a002c967899bcdd03a71
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-06 18:36:30 +0900

    203. Remove Linked List Elements Success Details Runtime: 68 ms, faster than 75.04% of Python3 online submissions for Remove Linked List Elements. Memory Usage: 17.1 MB, less than 90.79% of Python3 online submissions for Remove Linked List Elements.

---
+++ env/opt/leetcode/0200/0203_Remove_Linked_List_Elements.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        sentinel = ListNode(0)
+        sentinel.next = head
+        prev, curr = sentinel, head
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+        return sentinel.next
