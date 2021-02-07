9f76bf6330ccefa7b0841519a56da03f15188f83
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-08 08:05:11 +0900

    String

--- env/opt/basics/data/string.py
+++ env/opt/basics/data/string.py
@@ -1,33 +1,58 @@
 # coding: utf-8
-# Your code here!
-print("x" * 5)
+if __name__ == "__main__":
 
-str = "hello world"
-print(str[:3])
-print(str[3:])
-print(str[0:len(str):2])
+    print("I don't know")
+    print('I don\'t know')
+    # 改行
+    print(r'C:\names\name')
 
-print("ord", ord("％"))
-print("chr", chr(65285))
+    # 繰り返し
+    print("x" * 5)
 
-print(str.split())
-print(str.partition(" "))
+    # 文字列連結
+    print("Py" + "thon")
+    print("".join(["Py", "thon"]))
+    print("Hi" * 3 + ", Mike!")
+    s = ('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' 'BBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
+    print(s)
 
-print(str.count("l")) # l が何個あるか
-print("ascii", ascii(str))
-print("encode", str.encode()) #'b"hello world" バイト型
+    # スライス
+    str = "hello world"
+    print(str[:3])
+    print(str[3:])
+    print(str[0:len(str):2])
 
-print(str.find("l"))
-print(str.find("x"))
-print(str.rfind("l"))
-print(str.index("l"))
+    # ヒアドキュメント
+    print("######")
+    print("""\
+line1
+line2
+line3\
+    """)
+    print("######")
+ 
+    print("ord", ord("％"))
+    print("chr", chr(65285))
 
-x = b'abc'
-y = bytearray(x)
-print("bytearray", y[0])
+    print(str.split())
+    print(str.partition(" "))
 
-ans = eval("1+2")
-print(ans)
+    print(str.count("l")) # l が何個あるか
+    print("ascii", ascii(str))
+    print("encode", str.encode()) #'b"hello world" バイト型
 
-exec("a = 1 + 4")
-exec("print(a)")
+    print(str.find("l"))
+    print(str.find("x"))
+    print(str.rfind("l"))
+    print(str.index("l"))
+
+    x = b'abc'
+    y = bytearray(x)
+    print("bytearray", y[0])
+
+    # === EVAL
+    ans = eval("1+2")
+    print(ans)
+
+    exec("a = 1 + 4")
+    exec("print(a)")
