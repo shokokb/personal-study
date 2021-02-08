3b4a4fa8b3ccabcf9c2f0e6c05d39c19952398b1
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-08 21:30:56 +0900

    1288. Remove Covered Intervals Success Details Runtime: 88 ms, faster than 94.32% of Python3 online submissions for Remove Covered Intervals. Memory Usage: 14.8 MB, less than 79.55% of Python3 online submissions for Remove Covered Intervals.

--- env/opt/leetcode/1200/1288_Remove_Covered_Intervals.py
+++ env/opt/leetcode/1200/1288_Remove_Covered_Intervals.py
@@ -1,17 +1,11 @@
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-        # intervals.sort()
+        intervals.sort(key = lambda x: (x[0], -x[1]))
         # print(intervals)
-        is_necessary = [True for _ in range(len(intervals))]
-        for i in range(len(intervals)):
-            for j in range(0,i):
-                if not is_necessary:
-                    break
-                if intervals[j][0] <= intervals[i][0] and \
-                   intervals[i][1] <= intervals[j][1]:
-                    is_necessary[i] = False
-                if intervals[i][0] <= intervals[j][0] and \
-                   intervals[j][1] <= intervals[i][1]:
-                    is_necessary[j] = False
-        return is_necessary.count(True)
-        
+        prev_end = 0
+        count = 0
+        for _, end in intervals:
+            if end > prev_end:
+                count += 1
+                prev_end = end
+        return count
