75e2558c8ad867768b488bb2f5281bee39ec45d1
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-09 00:30:05 +0900

    ZIP関数

---
+++ env/opt/basics/zip.py
@@ -0,0 +1,9 @@
+# coding: UTF-8
+
+if __name__ == "__main__":
+
+    week = ['Sun', 'Tue', 'Wed']
+    fruits = ['apple', 'banana', 'nuts']
+
+    for week, fruit in zip(week, fruits):
+        print(week, fruit)
