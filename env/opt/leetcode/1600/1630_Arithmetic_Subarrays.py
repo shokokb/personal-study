f7f8fa274c7474fda767866bc7a07975b04fabc3
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-08 20:53:18 +0900

    1630. Arithmetic Subarrays uccess Details Runtime: 260 ms, faster than 24.11% of Python3 online submissions for Arithmetic Subarrays. Memory Usage: 14.6 MB, less than 8.79% of Python3 online submissions for Arithmetic Subarrays.

--- env/opt/leetcode/1600/1630_Arithmetic_Subarrays.py
+++ env/opt/leetcode/1600/1630_Arithmetic_Subarrays.py
@@ -5,8 +5,5 @@ class Solution:
             arr = nums[l[i]:r[i]+1]
             arr.sort()
             d = arr[1]-arr[0]
-            if all([arr[i+1]-arr[i] == d for i in range(0,len(arr)-1)]) :
-                ret.append(True)
-            else:
-                ret.append(False)
+            ret.append(all([arr[i+1]-arr[i] == d for i in range(0,len(arr)-1)]))
         return ret
