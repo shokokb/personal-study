26fb82d24e7c9f1c6a8de57499a432fffe488959
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-08 09:55:54 +0900

    Tuple

--- env/opt/basics/data/tuple.py
+++ env/opt/basics/data/tuple.py
@@ -1,13 +1,20 @@
 # coding: utf-8
-# Your code here!
 
-t = (1,"apple",2,"orange",3)
-print(type(t))
-print(len(t))
-for v in t:
-    print(v)
+if __name__ == "__main__":
+    t = (1,"apple",2,"orange",3)
+    print(type(t))
+    print(len(t))
+    for v in t:
+        print(v)
 
-# Tupleはインデックスを持つ
-t = ((1, "gold"), (2, "silver"), (3, "bronze"))
-for i, color in t:
-    print(i, color)
+    # Tupleはインデックスを持つ
+    t = ((1, "gold"), (2, "silver"), (3, "bronze"))
+    for i, color in t:
+        print(i, color)
+
+    # index, count しかない
+    # index
+    t = (1,"apple",2,"orange",3, "apple")
+    print(t.index(1))
+    # count
+    print(t.count("apple"))
