3344ba07c533d51fd009d77245bed4b0fb2ea04c
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-09 21:39:33 +0900

    Success Details Runtime: 604 ms, faster than 8.72% of Python3 online submissions for All Elements in Two Binary Search Trees. Memory Usage: 23 MB, less than 7.89% of Python3 online submissions for All Elements in Two Binary Search Trees.

--- env/opt/leetcode/1300/1305_All_Elements_in_Two_Binary_Search_Trees.py
+++ env/opt/leetcode/1300/1305_All_Elements_in_Two_Binary_Search_Trees.py
@@ -6,26 +6,8 @@
 #         self.right = right
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        ret = []
-        q1 = deque()
-        q1.append(root1)
-        if root1:
-            while q1:
-                node = q1.popleft()
-                if node.left:
-                    q1.append(node.left)
-                if node.right:
-                    q1.append(node.right)
-                ret.append(node.val)
-        if root2:
-            q2 = deque()
-            q2.append(root2)
-            while q2:
-                node = q2.popleft()
-                if node.left:
-                    q2.append(node.left)
-                if node.right:
-                    q2.append(node.right)
-                ret.append(node.val)
-        ret.sort()
-        return ret
+        def inorder (root):
+            return inorder(root.left) + [root.val] + inorder(root.right) if root else []
+        q1 = inorder(root1)
+        q2 = inorder(root2)
+        return sorted(q1 + q2)
