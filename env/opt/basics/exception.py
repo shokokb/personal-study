f3f1fe2fc08b6fab0c9ce4fdf0d73a4364283718
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-09 12:32:38 +0900

    Exception

--- env/opt/basics/exception.py
+++ env/opt/basics/exception.py
@@ -1,12 +1,18 @@
 # coding: UTF-8
 
-try :
-	print 10 / 0
-except ZeroDivisionError:
-	print "NG"
-else :
-	print "OK"
-finally : 
-	print "All done!"
+class UppercaseError(Exception):
+	pass
+
+if __name__ == "__main__":
+	try :
+		print(10 / 0)
+	except ZeroDivisionError:
+		print("NG")
+	except UppercaseError as e:
+		print(e)
+	else :
+		print("OK")
+	finally : 
+		print("clean up!")
 
 
