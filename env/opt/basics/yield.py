4e32b6c60cee887781a506b687208d3dabf6b00a
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-09 12:19:50 +0900

    Generator

--- env/opt/basics/yield.py
+++ env/opt/basics/yield.py
@@ -4,10 +4,17 @@ def myfunc():
     yield 'three'
 
 if __name__ == "__main__":
-    my = myfunc()
+        my = myfunc()
     print(next(my))
     print(next(my))
     print(next(my))
     # for x in myfunc():
     #     print(x)
     # print(myfunc())
+
+    g = (i for i in range(10))
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    print(next(g))
+    print(next(g))
