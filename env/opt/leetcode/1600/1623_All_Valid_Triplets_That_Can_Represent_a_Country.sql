143cfed58fa4026d5f75fec1382b7167d0218b99
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-09 18:55:25 +0900

    1623. All Valid Triplets That Can Represent a Country Success Details Runtime: 1722 ms, faster than 87.02% of MySQL online submissions for All Valid Triplets That Can Represent a Country. Memory Usage: 0B, less than 100.00% of MySQL online submissions for All Valid Triplets That Can Represent a Country.

---
+++ env/opt/leetcode/1600/1623_All_Valid_Triplets_That_Can_Represent_a_Country.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT 
+    a.student_name AS member_A, 
+    b.student_name AS member_B, 
+    c.student_name AS member_C
+FROM SchoolA AS a
+CROSS JOIN SchoolB AS b
+CROSS JOIN SchoolC AS c
+WHERE a.student_id <> b.student_id
+AND   b.student_id <> c.student_id
+AND   c.student_id <> a.student_id
+AND   a.student_name <> b.student_name
+AND   b.student_name <> c.student_name
+AND   c.student_name <> a.student_name
