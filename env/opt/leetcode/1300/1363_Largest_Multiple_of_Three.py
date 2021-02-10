9773b89d6e90f35db9586195519610feb400d41f
shoko.kobayashi <tmp-s-kobayashi@mercari-partner.com>
2021-02-10 13:20:59 +0900

    Success Details Runtime: 104 ms, faster than 65.89% of Python3 online submissions for Largest Multiple of Three. Memory Usage: 15.6 MB, less than 8.53% of Python3 online submissions for Largest Multiple of Three.

--- env/opt/leetcode/1300/1363_Largest_Multiple_of_Three.py
+++ env/opt/leetcode/1300/1363_Largest_Multiple_of_Three.py
@@ -1,12 +1,39 @@
 class Solution:
     def largestMultipleOfThree(self, digits: List[int]) -> str:
-        digits.sort(reverse = True)
-        for i in range(len(digits), 0, -1):
-            c = combinations(digits, i)
-            for comb in c:
-                if sum(comb) % 3 == 0:
-                    if all([x == 0 for x in comb]):
-                        return "0"
-                    return "".join(map(lambda x: str(x), list(comb)))
-        return ""
-        
+        group = defaultdict(lambda:[])
+        for n in digits:
+            group[n % 3].append(n)
+        group[1].sort()
+        group[2].sort()
+        mod = sum(digits) % 3
+        if mod == 2:
+            try:
+                group[2].pop(0)
+                ret = sorted(reduce(lambda a, b: a + b, group.values()), reverse=True)
+                return "".join([str(x) for x in ret])
+            except:
+                try:
+                    group[1].pop(0)
+                    group[1].pop(0)
+                    ret = sorted(reduce(lambda a, b: a + b, group.values()), reverse=True)
+                    return "".join([str(x) for x in ret])
+                except:
+                    return ""
+        elif mod == 1:
+            try:
+                group[1].pop(0)
+                ret = sorted(reduce(lambda a, b: a + b, group.values()), reverse=True)
+                return "".join([str(x) for x in ret])
+            except:
+                try:
+                    group[2].pop(0)
+                    group[2].pop(0)
+                    ret = sorted(reduce(lambda a, b: a + b, group.values()), reverse=True)
+                    return "".join([str(x) for x in ret])
+                except:
+                    return ""
+        else:
+            ret = sorted(digits, reverse=True)
+            if all([x == 0 for x in digits]):
+                return "0"
+            return "".join([str(x) for x in ret])
