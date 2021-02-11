2d587cbbb26a0a8266e0b497a027504c0df1d607
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-12 00:55:02 +0900

    heapq

---
+++ env/opt/basics/data/heaqp.py
@@ -0,0 +1,23 @@
+# coding: UTF-8
+
+import heapq
+
+if __name__ == "__main__":
+    a = [1, 6, 8, 0, -1]
+    heapq.heapify(a)
+    heapq.heappush(a, 2)
+    while a:
+        print(heapq.heappop(a))
+    print("len", len(a))
+
+    print("====")
+
+    a = [1, 6, 8, 0, -1]
+    a = list(map(lambda x: x * -1, a))
+    heapq.heapify(a)
+    heapq.heappush(a, 2)
+    while a:
+        print(heapq.heappop(a) * -1)
+    print("len", len(a))
+
+
