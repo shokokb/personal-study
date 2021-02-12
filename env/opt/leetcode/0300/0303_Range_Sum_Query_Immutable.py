f7eb997b604d0031489d5c4436b0b0c600bdf899
shoko.kobayashi <tmp-s-kobayashi@mercari-partner.com>
2021-02-12 10:43:44 +0900

    303. Range Sum Query - Immutable Success Details Runtime: 76 ms, faster than 85.63% of Python3 online submissions for Range Sum Query - Immutable. Memory Usage: 17.8 MB, less than 48.63% of Python3 online submissions for Range Sum Query - Immutable.

--- env/opt/leetcode/0300/0303_Range_Sum_Query_Immutable.py
+++ env/opt/leetcode/0300/0303_Range_Sum_Query_Immutable.py
@@ -1,10 +1,12 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
+        self.sums = [0 for _ in nums ]
+        for i, n in enumerate(nums):
+            self.sums[i] = n if i == 0 else self.sums[i-1] + n
 
     def sumRange(self, i: int, j: int) -> int:
-        return sum(self.nums[i:j+1])
+        return self.sums[j] if i == 0 else self.sums[j] - self.sums[i-1]
 
 
 # Your NumArray object will be instantiated and called as such:
