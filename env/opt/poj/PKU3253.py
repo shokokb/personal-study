0487b2b9f21450c1ec184e2802dec59378fc10d3
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-13 15:02:23 +0900

    PKU3253

--- env/opt/poj/PKU3253.py
+++ env/opt/poj/PKU3253.py
@@ -3,36 +3,20 @@
 import heapq
 
 def solve():
-    # 補給回数
     ans = 0
-    N = 4
-    L = 25
-    P = 10
-    A = [10, 14, 20, 21, L]
-    B = [10, 5, 2, 4, 0]
-    b = []
-    b = list(map(lambda x: x * -1, b))
-    heapq.heapify(b)
-    # 現在いる場所
-    pos = 0
-    # タンクの量
-    tank = P
-    for i in range(N):
-        # 次に進む距離
-        d = A[i] - pos
-        # ガソリンが不足する
-        while tank < d:
-            if b:
-                new_oil = heapq.heappop(b) * -1
-                print(new_oil)
-                tank += new_oil
-                ans += 1
-            else:
-                return -1
-        pos = A[i]
-        tank -= d
-        heapq.heappush(b, -B[i])
-    return ans
+    L = [8,8,5]
+    # L = list(map(lambda x: -x, L))
+    heapq.heapify(L)
+    print(L)    
+    while L:
+        if len(L) == 1:
+            return ans
+        l1 = heapq.heappop(L)
+        l2 = heapq.heappop(L)
+        ans += l1 + l2
+        print("ans", l1, l2, l1 + l2)
+        heapq.heappush(L, l1 + l2)
 
 if __name__ == "__main__":
     print("solve", solve())
+
