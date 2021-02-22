da0d04b514cff96c527f513ff67d592c6ec1dc36
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-22 18:25:58 +0900

    277. Find the Celebrity Success Details Runtime: 1904 ms, faster than 45.67% of Python3 online submissions for Find the Celebrity. Memory Usage: 14.5 MB, less than 75.96% of Python3 online submissions for Find the Celebrity.

--- env/opt/leetcode/0200/277_Find_the_Celebrity.py
+++ env/opt/leetcode/0200/277_Find_the_Celebrity.py
@@ -3,12 +3,18 @@
 # def knows(a: int, b: int) -> bool:
 
 class Solution:
+    def isCelebrity(self, i, n):
+        for j in range(n):
+            if i == j:
+                continue
+            if knows(i, j) or not knows(j, i):
+                return False
+        return True
     def findCelebrity(self, n: int) -> int:
-        for a in range(n):
-            bs = list(range(n))
-            del bs[a]
-            ans = True
-            if all(map(lambda x: knows(x, a), bs)) and \
-                   all(map(lambda x: not knows(a, x), bs)):
-                return a
+        celebrity_candicate = 0
+        for i in range(1, n):
+            if knows(celebrity_candicate, i):
+                celebrity_candicate = i
+        if self.isCelebrity(celebrity_candicate, n):
+            return celebrity_candicate
         return -1
