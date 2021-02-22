be8a975934d2d195e658a72f0def393dafcf8aa3
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-22 19:09:22 +0900

    997. Find the Town Judge Success Details Runtime: 724 ms, faster than 82.19% of Python3 online submissions for Find the Town Judge. Memory Usage: 19.1 MB, less than 5.00% of Python3 online submissions for Find the Town Judge.

--- env/opt/leetcode/0900/0997_Find_the_Town_Judge.py
+++ env/opt/leetcode/0900/0997_Find_the_Town_Judge.py
@@ -2,12 +2,13 @@ class Solution:
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
         if len(trust) < N - 1:
             return -1
-        indegree = [0 for _ in range(N + 1)]
-        outdegree = [0 for _ in range(N + 1)]
+        trust_score = [0 for _ in range(N + 1)]
+
         for a, b in trust:
-            outdegree[a] += 1
-            indegree[b] += 1
+            trust_score[a] -= 1
+            trust_score[b] += 1
+            
         for i in range(1, N + 1):
-            if indegree[i] == N - 1 and outdegree[i] == 0:
+            if trust_score[i] == N - 1:
                 return i
         return -1
