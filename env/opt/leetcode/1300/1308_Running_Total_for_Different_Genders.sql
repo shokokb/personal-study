1620522d4208821bb17ede43738f6d06754056e1
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2021-02-22 19:42:17 +0900

    1308. Running Total for Different Genders Success Details Runtime: 503 ms, faster than 86.48% of MySQL online submissions for Running Total for Different Genders. Memory Usage: 0B, less than 100.00% of MySQL online submissions for Running Total for Different Genders.

--- env/opt/leetcode/1300/1308_Running_Total_for_Different_Genders.sql
+++ env/opt/leetcode/1300/1308_Running_Total_for_Different_Genders.sql
@@ -2,10 +2,5 @@
 SELECT 
     gender, 
     day, 
-    (SELECT sum(score_points) FROM Scores AS s2 
-        WHERE s2.gender = s1.gender 
-        AND   s2.day <= s1.day) 
-    as total
-FROM Scores AS s1
-GROUP BY gender, day
-ORDER BY gender, day
+    SUM(score_points) OVER (partition by gender ORDER BY gender, day) AS total
+FROM Scores
