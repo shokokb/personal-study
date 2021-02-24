93886567c44f4acc96fb840d5bec650b938fed57
shoko.kobayashi <tmp-s-kobayashi@mercari-partner.com>
2021-02-24 12:02:36 +0900

    240. Search a 2D Matrix II Success Details Runtime: 152 ms, faster than 97.87% of Python3 online submissions for Search a 2D Matrix II. Memory Usage: 20.6 MB, less than 31.15% of Python3 online submissions for Search a 2D Matrix II.

--- env/opt/leetcode/0200/0240_Search_a_2D_Matrix_II.py
+++ env/opt/leetcode/0200/0240_Search_a_2D_Matrix_II.py
@@ -1,17 +1,30 @@
 class Solution:
-    def binary_search(self, row: List[int], target:int) -> bool:
-        if len(row) == 0:
-            return False
-        mid = len(row) // 2
-        if target == row[mid]:
-            return True
-        elif target < row[mid]:
-            return self.binary_search(row[:mid], target)
-        else:
-            return self.binary_search(row[mid+1:], target)
+    def binary_search(self, matrix:List[List[int]], \
+                      target:int, start:int, vertical:bool) -> bool:
+        low = start
+        high = len(matrix[0]) - 1 if vertical else len(matrix) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if vertical:
+                if matrix[start][mid] < target:
+                    low = mid + 1
+                elif matrix[start][mid] > target:
+                    high = mid - 1
+                else:
+                    return True
+            else:
+                if matrix[mid][start] < target:
+                    low = mid + 1
+                elif matrix[mid][start] > target:
+                    high = mid - 1
+                else:
+                    return True
+        return False
         
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for row in matrix:
-            if self.binary_search(row, target):
+        for i in range(min(len(matrix), len(matrix[0]))):
+            v = self.binary_search(matrix, target, i, True)
+            h = self.binary_search(matrix, target, i, False)
+            if v or h:
                 return True
         return False
