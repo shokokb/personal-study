41442f77b09b16123b0f4d76f2116e715c801b56
shoko.kobayashi <tmp-s-kobayashi@mercari-partner.com>
2021-03-02 15:03:49 +0900

    456. 132 Pattern(Better Brute Force)

--- env/opt/leetcode/0400/0456_132_Pattern.py
+++ env/opt/leetcode/0400/0456_132_Pattern.py
@@ -1,8 +1,9 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
-                    if nums[i] < nums[k] < nums[j]:
-                        return True
+        min_i = math.inf
+        for j in range(len(nums) - 1):
+            min_i = min(min_i, nums[j])
+            for k in range(j + 1, len(nums)):
+                if min_i < nums[k] < nums[j]:
+                    return True
         return False
