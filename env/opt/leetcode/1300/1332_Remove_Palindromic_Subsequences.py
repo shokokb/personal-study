7a55f618573dfadeaf24cb05ddd7902c72e5dd7e
shoko.kobayashi <tmp-s-kobayashi@mercari-partner.com>
2021-03-09 15:53:14 +0900

    1332. Remove Palindromic Subsequences

--- env/opt/leetcode/1300/1332_Remove_Palindromic_Subsequences.py
+++ env/opt/leetcode/1300/1332_Remove_Palindromic_Subsequences.py
@@ -1,7 +1,17 @@
 class Solution:
     def removePalindromeSub(self, s: str) -> int:
+        def is_palindrome(s:str) -> bool:
+            left = 0
+            right = len(s) - 1
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+        
         if s == "":
             return 0
-        if s == s[::-1]:
+        if is_palindrome(s):
             return 1
         return 2
