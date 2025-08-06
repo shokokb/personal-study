1d67e994a4a8e263597a9bde276d6d438be785ed
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-06 16:35:29 +0900

    Create john_6_1_14.py

---
+++ src/bible/john_6_1_14.py
@@ -0,0 +1,11 @@
+# coding : UTF-8
+
+def main():
+    bread = int(input("How many 🍞🍞🍞🍞🍞?"))
+    fish = int(input("How many 🐟🐟?"))
+    print("😊" if bread == 5 and fish == 2 else "😅?" )
+    
+if __name__ == "__main__":
+    main()
+
+# John 6:1-14
