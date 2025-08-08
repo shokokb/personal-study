07c5eacfd109a7395909b02d0763739f35fcee42
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-08 20:18:27 +0900

    Update step-8-yield.py

--- src/celebrate-recovery/step-8-yield.py
+++ src/celebrate-recovery/step-8-yield.py
@@ -1,17 +1,26 @@
 # coding : UTF-8
 
-def pray():
+def tell_good_news():
+    messages = [
+        "God is with you💓",
+        "You are the light of the world💡 - Matthew 5:14",
+        "Your sins are forgiven✝️ - Luke 7:48",
+        "I am with you always🌈 - Matthew 28:20",
+    ]
     while True:
-        yield "God is with you🌏"
+        for message in messages:
+            yield message
 
 def main() :
-    survive = pray()
+    alive = tell_good_news()
     try : 
-        print(next(survive))
-        print(next(survive))
-        print(next(survive))
+        print(next(alive))
+        print(next(alive))
+        print(next(alive))
     except StopIteration:
         print("Now, you are in the Heaven👼")
 
 if __name__ == "__main__":
     main()
+
+# Matthew 5:10
