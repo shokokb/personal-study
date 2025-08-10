2519f0e892851107723392ab238c468db7bc832f
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-10 13:27:48 +0900

    Update bubble_sort.py

--- src/sort/bubble_sort.py
+++ src/sort/bubble_sort.py
@@ -5,9 +5,10 @@
 # Time complexity: O(n^2) - inefficient for large datasets
 # Spece complexity: O(1) - constant space
 def bubble_sort(l):
-    for idx in range(len(l)):
+    n = len(l)
+    for idx in range(n):
         # Elements before index idx (l[:idx]) are already sorted.
-        for i in range(len(l) - 1, idx, -1):
+        for i in range(n - 1, idx, -1):
             if l[i] < l[i - 1]: # compare l[i - 1] ans l[i] as neighbors
                 l[i], l[i - 1] = l[i - 1], l[i] 
     return l
