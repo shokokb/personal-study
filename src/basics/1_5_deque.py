b45ea650b619a4f43948271cb2f0961036c8117b
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-14 11:27:57 +0900

    Update 1_5_deque.py

--- src/basics/1_5_deque.py
+++ src/basics/1_5_deque.py
@@ -1,11 +1,17 @@
+# coding : UTF-8
+
 from collections import deque
 
 def main():
-    q = deque([0, 1, 2, 3])
-    q.append(5)         # O(1)
-    item = q.popleft()  # O(1)
+    queue = deque()
+    queue.append("Blue")    # O(1)
+    queue.append("Green")   # O(1)
+    queue.append("Red")     # O(1)
+    item = queue.popleft()  # O(1)
     print(item)
-    item = q.pop()      # O(1)
+    item = queue.popleft()      # O(1)
+    print(item)
+    item = queue.popleft()      # O(1)
     print(item)
 
 if __name__ == "__main__":
