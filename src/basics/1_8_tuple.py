29e763b16ff6e14159a919947a45fc1b7e526e7b
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-14 15:53:22 +0900

    Create 1_8_tuple.py

---
+++ src/basics/1_8_tuple.py
@@ -0,0 +1,12 @@
+# coding : UTF-8
+
+def main():
+    fruits = ("Apple", "Grape", "Orange", "Apple")
+    for fruit in fruits:
+        print(fruit)
+    print("===")
+    for i in range(len(fruits)):
+        print(fruits[i])
+
+if __name__ == "__main__":
+    main()
