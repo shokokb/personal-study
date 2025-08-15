65f476d74fde06f9c03b57b89dcd9968380d9277
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-15 20:31:14 +0900

    Update 342_power_of_four.py

--- src/leetcode/342_power_of_four.py
+++ src/leetcode/342_power_of_four.py
@@ -5,19 +5,23 @@ import math
 # An integer n is a power of four, if there exists an integer x such that n == 4^x.
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n < 0:
-            return False
-
+        # if n < 0:
+        #     return False
         # number = 1
-
         # while number <= n:
         #     if number == n:
         #         return True
         #     number *= 4
         # return False 
 
-        log_n = math.log(n, 4)
-        return log_n.is_integer()
+        # ===
+        # if n < 0:
+        #     return False
+        # log_n = math.log(n, 4)
+        # return log_n.is_integer()
+
+        #===
+        return n > 0 and (n & (n - 1) == 0) and (n - 1) % 3 == 0
 
 class TestSolution(unittest.TestCase):
     def testIsPowerOfFour(self):
