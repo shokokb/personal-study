09c4895968098794829945adc8ee8a3f0797f9a9
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-18 11:29:44 +0900

    Update 13_roman_to_integer.py

--- src/leetcode/13_roman_to_integer.py
+++ src/leetcode/13_roman_to_integer.py
@@ -1,36 +1,58 @@
 # coding : UTF-8
 import unittest
+
 class Solution:
 
-    def romanToInt(self, s: str) -> int:
-        ret = 0
-        l = list(s)
-        char_to_value = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100, 
-            'D': 500,
-            'M': 1000
-        }
-        place_before = {
-            'I': '',
-            'V': 'I',
-            'X': 'I',
-            'L': 'X',
-            'C': 'X', 
-            'D': 'C',
-            'M': 'C'
-        }
-        while l:
-            c = l.pop()
-            ret += char_to_value[c]
-            if l and l[-1] == place_before[c]:
-                c = l.pop()
-                ret -= char_to_value[c]
+    # def romanToInt(self, s: str) -> int:
+    #     ret = 0
+    #     l = list(s)
+    #     char_to_value = {
+    #         'I': 1,
+    #         'V': 5,
+    #         'X': 10,
+    #         'L': 50,
+    #         'C': 100, 
+    #         'D': 500,
+    #         'M': 1000
+    #     }
+    #     place_before = {
+    #         'I': '',
+    #         'V': 'I',
+    #         'X': 'I',
+    #         'L': 'X',
+    #         'C': 'X', 
+    #         'D': 'C',
+    #         'M': 'C'
+    #     }
+    #     while l:
+    #         c = l.pop()
+    #         ret += char_to_value[c]
+    #         if l and l[-1] == place_before[c]:
+    #             c = l.pop()
+    #             ret -= char_to_value[c]
+        
+    #     return ret
 
-        return ret
+    # O(n)
+    def romanToInt(self, s: str) -> int:
+        total = 0
+        prev_value = 0
+
+        char_to_value = {
+            'I': 1,  'V': 5,   'X': 10,
+            'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
+
+        for c in s[::-1]:           # O(n)
+            value = char_to_value[c]    # O(1)
+            if value < prev_value:
+                total -= value
+            else :
+                total += value
+                # print("hello", total)
+            prev_value = value
+
+        return total
 
 class TestSolution(unittest.TestCase):
     def testRomanToInt(self):
