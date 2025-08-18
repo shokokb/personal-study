1acda3c1021c825e4de195b95838544a17c60c80
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-18 09:28:56 +0900

    Update 7_reverse_integer.py

--- src/leetcode/7_reverse_integer.py
+++ src/leetcode/7_reverse_integer.py
@@ -6,48 +6,43 @@ import unittest
 # If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 # Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 class Solution:
-    def reverse(self, x: int) -> int:
+    def reverse(self, x: int, plan:int = 0) -> int:
         ret = 0
 
         INT_MIN = -2**31
         INT_MAX = 2**31 - 1
 
-        # 1.文字列で反転する方法
-        # str() で文字列にして、[::-1] で反転
-        text = str(x)[::-1]
-         
-        # 符号は別にして、最後に戻す
-        if text[-1] == "-":
-            text = "-" + text[:-1]
-        ret = int(text)
+        if plan == 1:
+            # 1.Convert the value to a string, and then revert it 
+            # str() で文字列にして、[::-1] で反転
+            text = str(x)[::-1]
+            
+            # Put a sign at the top
+            if text[-1] == "-":
+                text = "-" + text[:-1]
+            ret = int(text)
 
-        # int() に変換後、範囲チェック
-        if ret < INT_MIN or INT_MAX > ret:
+            # Check the range of the return value
+            if ret < INT_MIN or INT_MAX < ret:
+                ret = 0
+
+            return ret
+        else :
+            # 2. Reverse the number mathmatically (decimal surplus and division)
             ret = 0
+            sign = -1 if x < 0 else 1
+            absolute_x = x * sign 
 
-        return ret
+            while absolute_x > 0:
+                ret = ret * 10 + absolute_x % 10
+                absolute_x = absolute_x // 10
+            ret = sign *  ret
 
-        # 2. 数学的に反転する方法（10進の剰余・除算）
-        # while ループで n % 10 を取り出して末尾に追加
-        # n //= 10 で1桁ずつ削る
-        # 新しい数字を構築しながら範囲チェック
-        # 実装はまだ
-        # sign = 1
-        # number = 0
-        
-        # if x < 0:
-        #     sign = -1
-        #     x *= -1
+            # Check the range of the return value
+            if ret < INT_MIN or INT_MAX < ret:
+                ret = 0
 
-        # n = 0
-
-        # while x > 0:
-        #     mod = x % 10
-        #     x = x // 10
-        #     number += mod * 10 ** n
-        #     n += 1
-
-        return ret
+            return ret 
             
 class TestSolution(unittest.TestCase):
     def testReverse(self):
