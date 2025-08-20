8ae662b02ac264320e40459c260b3581c60b3876
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-20 21:07:45 +0900

    Create 14_longest_common_prefix.py

---
+++ src/leetcode/14_longest_common_prefix.py
@@ -0,0 +1,27 @@
+# coding;UTF-8
+
+import unittest
+from typing import List 
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # 1. Easiest Way (Linear Search)
+        min_len = min([len(s) for s in strs])
+        for i in range(min_len):    # O(n)
+            char_set = set(s[i] for s in strs)
+            if len(char_set) != 1:
+                return strs[0][:i]
+        return strs[0][:min_len]     
+
+class TestSolution(unittest.TestCase):
+    def testLongestCommonPrefix(self):
+        s = Solution()
+        self.assertEqual("fl", s.longestCommonPrefix(["flower","flow","flight"]))
+        self.assertEqual("", s.longestCommonPrefix(["dog","racecar","car"]))
+
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
