18d09c3420092541bf56035480d571e5ccf16c3f
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-28 16:05:47 +0900

    Update 198_house_robber.py

--- src/leetcode/198_house_robber.py
+++ src/leetcode/198_house_robber.py
@@ -4,7 +4,7 @@ from typing import List
 import unittest
 
 # Time complexity : O(n)
-# Space complexity : O(n)
+# Space complexity : O(n)->O(1)
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if not nums:
@@ -12,14 +12,20 @@ class Solution:
 
         n = len(nums)
 
-        dp = [0] * n
+        # O(1)
+        prev1, prev2 = 0,0
+        for num in nums:
+            current = max(prev1, prev2 + num)
+            prev2, prev1 = prev1, current
+        return prev1
 
-        for i in range(n):
-            prev1 = dp[i-1] if i >= 1 else 0
-            prev2 = (dp[i-2] if i >= 2 else 0)+nums[i]
-            dp[i] = max(prev1, prev2)
+        # dp = [0] * n
+        # for i in range(n):
+        #     prev1 = dp[i-1] if i >= 1 else 0
+        #     prev2 = (dp[i-2] if i >= 2 else 0)+nums[i]
+        #     dp[i] = max(prev1, prev2)
 
-        return dp[n-1]
+        # return dp[n-1]
 
 class TestSolution(unittest.TestCase):
     def testRob(self):
