37d82916791001df03c1083dd21325e6a5da575a
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-28 11:29:40 +0900

    Update merge_sort.py

--- src/sort/merge_sort.py
+++ src/sort/merge_sort.py
@@ -4,11 +4,25 @@ import unittest
 
 class MergeSort:
     @staticmethod
-    def sort():
-        return []
+    def sort(l):
+        if len(l) <= 1:
+            return l
+
+        left_arr = l[:len(l)//2]
+        right_arr = l[len(l)//2:]
+
+        sorted_left = MergeSort.sort(left_arr)
+        sorted_right = MergeSort.sort(right_arr)
+        print(left_arr, right_arr, sorted_left, sorted_right)
+
+        res = []
+        while sorted_left and sorted_right:
+            res.append(sorted_left.pop(0) if sorted_left[0] < sorted_right[0] else sorted_right.pop(0)) # pop(0)=O(n)
+        res += sorted_left if sorted_left else sorted_right
+
+        return res
 
 class TestMergeSort(unittest.TestCase):
-
     def test_sort(self):
         self.assertEqual([1, 2, 3, 4, 6], MergeSort.sort([1, 3, 2, 6, 4]))
 
