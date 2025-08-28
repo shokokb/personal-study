a9a3e8f55464bb5d3088b5a6bd809c9dfbc0fa0a
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-28 11:51:12 +0900

    Update merge_sort.py which runs O(nlogn)

--- src/sort/merge_sort.py
+++ src/sort/merge_sort.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+# Time complexity: O(nlog(n))
 class MergeSort:
     @staticmethod
     def sort(l):
@@ -12,14 +13,26 @@ class MergeSort:
         left_arr = l[:mid]
         right_arr = l[mid:]
 
-        sorted_left = MergeSort.sort(left_arr)
+        sorted_left = MergeSort.sort(left_arr)      # O(log(n)
         sorted_right = MergeSort.sort(right_arr)
-        print(left_arr, right_arr, sorted_left, sorted_right)
+        # print(left_arr, right_arr, sorted_left, sorted_right)
 
         res = []
-        while sorted_left and sorted_right:
-            res.append(sorted_left.pop(0) if sorted_left[0] < sorted_right[0] else sorted_right.pop(0)) # pop(0)=O(n)
-        res += sorted_left if sorted_left else sorted_right
+        i, j = 0, 0
+        while i < len(sorted_left) and j < len(sorted_right):   # O(n)
+            if sorted_left[i] < sorted_right[j]:
+                res.append(sorted_left[i])
+                i += 1
+            else:
+                res.append(sorted_right[j])
+                j += 1
+        if i < len(sorted_left):
+            res += sorted_left[i:]
+        if j < len(sorted_right):
+            res += sorted_right[j:]   
+        # while sorted_left and sorted_right:         # O(n)
+        #     res.append(sorted_left.pop(0) if sorted_left[0] < sorted_right[0] else sorted_right.pop(0)) # pop(0)=O(n)
+        # res += sorted_left if sorted_left else sorted_right
 
         return res
 
