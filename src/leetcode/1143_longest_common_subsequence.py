7f80afaefb54cc46905fe9d2669e751873c17c8e
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-08-30 08:24:32 +0900

    Update 1143_longest_common_subsequence.py
    Space Complaxity : O(m*n) -> O(m)

--- src/leetcode/1143_longest_common_subsequence.py
+++ src/leetcode/1143_longest_common_subsequence.py
@@ -3,20 +3,34 @@
 import unittest
 
 class Solution:
-    # Time Complexity = O(mn)
-    # Space Complexity = O(mn)
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # # Time Complexity = O(mn)
+        # # Space Complexity = O(mn)
+        # n, m = len(text1), len(text2)
+        # dp = [[0] * (m+1) for _ in range(n+1)]
+        # for i in range(1, n+1):
+        #     for j in range(1, m+1):
+        #         if text1[i-1] == text2[j-1]:
+        #             dp[i][j] = dp[i-1][j-1]+1
+        #         else:
+        #             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        # # print(dp)
+        # return dp[n][m]
+
+        # Time Complexity = O(mn)
+        # Space Complexity = O(m) dp[2][0...m]
         n, m = len(text1), len(text2)
-        dp = [[0] * (m+1) for _ in range(n+1)]
+        prev = [0] * (m+1) # dp[i-1]
+        curr = [0] * (m+1) # dp[i]
         for i in range(1, n+1):
             for j in range(1, m+1):
                 if text1[i-1] == text2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]+1
+                    curr[j] = prev[j-1] + 1
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        # print(dp)
-        return dp[n][m]
-        # TODO : 2D array -> 1D array
+                    curr[j] = max(prev[j], curr[j-1])
+            prev = curr
+        return curr[m]
+
 
 class TestSolution(unittest.TestCase):
     def testLongestCommonSubsequence(self):
