1ff40ab91373752c6b8fc1a27dba0760bce07c93
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-09-04 22:11:23 +0900

    Update 64_minimum_path_sum.py

--- src/leetcode/dp/64_minimum_path_sum.py
+++ src/leetcode/dp/64_minimum_path_sum.py
@@ -7,20 +7,31 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        dp = [[0] * n for _ in range(m)]
-        # print(dp)
+        # dp = [[0] * n for _ in range(m)]
+        # # print(dp)
 
-        dp[0][0] = grid[0][0]
+        # dp[0][0] = grid[0][0]
+        # for j in range(1, n):
+        #     dp[0][j] = dp[0][j-1] + grid[0][j]
+
+        # for i in range(1, m):
+        #     dp[i][0] = dp[i-1][0]+grid[i][0]
+        #     for j in range(1, n):
+        #         dp[i][j] = min(dp[i-1][j]+grid[i][j], dp[i][j-1]+grid[i][j])
+        # # print(dp)
+
+        # return dp[-1][-1]
+
+        dp = [0] * n
+        dp[0] = grid[0][0]
         for j in range(1, n):
-            dp[0][j] = dp[0][j-1] + grid[0][j]
-
+            dp[j] = dp[j-1] + grid[0][j]
         for i in range(1, m):
-            dp[i][0] = dp[i-1][0]+grid[i][0]
+            dp[0] += grid[i][0]
             for j in range(1, n):
-                dp[i][j] = min(dp[i-1][j]+grid[i][j], dp[i][j-1]+grid[i][j])
+                dp[j] = min(dp[j], dp[j-1]) +grid[i][j]
         # print(dp)
-
-        return dp[-1][-1]
+        return dp[-1]
 
 class TestSolution(unittest.TestCase):
     def testMinPathSum(self):
