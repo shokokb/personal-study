acd9a4570a2582325cf309b91c458d973ed50578
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-09-04 20:20:17 +0900

    Create 63_unique_paths_II.py

---
+++ src/leetcode/dp/63_unique_paths_II.py
@@ -0,0 +1,38 @@
+# coding : UTF-8
+
+import unittest
+from typing import List
+
+class Solution:
+    # Time Complexity = O(m*n)
+    # Space Complexity = O(m*n)
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+
+        dp = [[0] * n for _ in range(m)]
+        # print(dp)
+
+        dp[0] = [0 if obstacleGrid[0][j] == 1 else 1 for j in range(n)]
+        for i in range(1,m):
+            dp[i][0] = 0 if obstacleGrid[i][0] == 1 else 1
+            for j in range(1,n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        # print(dp)
+
+        return dp[-1][-1]
+
+class TestSolution(unittest.TestCase):
+    def testUniquePathsWithObstacles(self):
+        s = Solution()
+        self.assertEqual(2, s.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
+        self.assertEqual(1, s.uniquePathsWithObstacles([[0,1],[0,0]]))
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
