43a7b0ad56fec08e5315834e3f6d9f29e03d8e12
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-09-04 15:02:23 +0900

    Create 213_house_robber_II.py

---
+++ src/leetcode/dp/213_house_robber_II.py
@@ -0,0 +1,42 @@
+# coding : UTF-8
+
+import unittest
+from typing import List
+
+class Solution:
+    def rob_part(self, nums: List[int]):
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        
+        n = len(nums)
+        dp = [0] * n
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, n):
+            print(i, i-1, i-2)
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+
+        return dp[-1]
+
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        left  = self.rob_part(nums[:-1])
+        right = self.rob_part(nums[1:])
+        return max(left, right)
+
+class TestSolution(unittest.TestCase):
+    def testRob(self):
+        s = Solution()
+        self.assertEqual(3,s.rob([2,3,2]))
+        self.assertEqual(4,s.rob([1,2,3,1]))
+        self.assertEqual(3,s.rob([1,2,3]))
+        self.assertEqual(1,s.rob([1]))
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
