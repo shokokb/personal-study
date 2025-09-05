3617e573c422f8a32605aeb65f2b8c149027c7bf
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-09-05 18:27:13 +0900

    Update 559_maximum_depth_of_n-ary_tree.py

--- src/leetcode/dfs/559_maximum_depth_of_n-ary_tree.py
+++ src/leetcode/dfs/559_maximum_depth_of_n-ary_tree.py
@@ -25,6 +25,9 @@ class Node:
     def to_node(values):
         n = len(values) 
 
+        if n == 0:
+            return None
+
         root = Node(values[0])
         queue = deque([root])
 
@@ -37,17 +40,29 @@ class Node:
                 c = Node(val)
                 queue.append(c)
                 parent.children.append(c)
-                print("DEBUG", parent.val, [c.val for c in parent.children])
+                # print("DEBUG", parent.val, [c.val for c in parent.children])
         return root
 
 class Solution:
     def maxDepth(self, root: 'Node') -> int:
+        # if root is None:
+        #     return 0
+        # if not root.children:
+        #     return 1
+        # # print("[DEBUG]:", root.val, [c.val for c in root.children])
+        # return max([self.maxDepth(c) for c in root.children])+1
         if root is None:
             return 0
-        if not root.children:
-            return 1
-        # print("[DEBUG]:", root.val, [c.val for c in root.children])
-        return max([self.maxDepth(c) for c in root.children])+1
+
+        stack = [(root, 1)]
+        max_depth = 0
+
+        while stack:
+            node, depth = stack.pop()
+            max_depth = max(max_depth, depth)
+            for c in node.children:
+                stack.append((c, depth+1))
+        return max_depth
 
 class TestSolution(unittest.TestCase):
     def testMaxDepth(self):
@@ -58,6 +73,9 @@ class TestSolution(unittest.TestCase):
 
         n = Node.to_node([1,None,2,3,4,5,None,None,6,7,None,8,None,9,10,None,None,11,None,12,None,13,None,None,14])
         self.assertEqual(5, s.maxDepth(n))
+
+        n = Node.to_node([])
+        self.assertEqual(0, s.maxDepth(n))
 
 def main():
     unittest.main()
