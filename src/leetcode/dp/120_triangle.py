a400be9985c126411be82bda8a2d400b40c923b7
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-09-05 12:56:13 +0900

    Update 120_triangle.py

--- src/leetcode/dp/120_triangle.py
+++ src/leetcode/dp/120_triangle.py
@@ -5,6 +5,7 @@ from typing import List
 class Solution:
     # n = len(triangle)
     # Time Complexity = O(n^2)
+    # Space Complexity = O(n)
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         # dp = []
         # for i in range(len(triangle)):
@@ -25,20 +26,19 @@ class Solution:
         dp = []
         for i in range(len(triangle)):
             row = triangle[i]
-            dp_in_row = []
+            new_dp = [0] * len(row)
             if i == 0:
                 dp = [row[i]]
             else :
                 for j in range(len(row)):
                     if j == 0 :
-                        dp_in_row.append(dp[0]+row[j])
+                        new_dp[j] = dp[0]+row[j]
                     elif j == len(row)-1:
-                        dp_in_row.append(dp[-1]+row[j])
+                        new_dp[j] = dp[-1]+row[j]
                     else:
-                        dp_in_row.append(min(dp[j-1], dp[j])+row[j])  
-                dp = dp_in_row
+                        new_dp[j] = min(dp[j-1], dp[j])+row[j]  
+                dp = new_dp
         return min(dp) # O(nlogn)
-
 
 class TestSolution(unittest.TestCase):
     def testMinimumTotal(self):
