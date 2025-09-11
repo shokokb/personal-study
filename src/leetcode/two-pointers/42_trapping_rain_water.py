45eaeb8c4a081e63a06b4f045dd7878ed1849d43
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-09-11 18:52:58 +0900

    Update 42_trapping_rain_water.py

--- src/leetcode/two-pointers/42_trapping_rain_water.py
+++ src/leetcode/two-pointers/42_trapping_rain_water.py
@@ -7,8 +7,18 @@ class Solution:
         res = 0
 
         n = len(height)
-        left_max = [max(height[:i+1]) for i in range(n)]
-        right_max = [max(height[i:]) for i in range(n)]
+
+        # left_max = [max(height[:i+1]) for i in range(n)]
+        left_max = [0] * n
+        left_max[0] = height[0]
+        for i in range(1, n):
+            left_max[i] = max(left_max[i-1], height[i])
+        
+        # right_max = [max(height[i:]) for i in range(n)]
+        right_max = [0] * n
+        right_max[n-1] = height[n-1]
+        for i in range(n-2, -1, -1):
+            right_max[i] = max(right_max[i+1], height[i])
 
         for i in range(n):
             res += min(left_max[i], right_max[i]) - height[i]
