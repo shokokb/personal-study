3256f2de6212b1fd388faef30ade5132ec834383
Shoko KOBAYASHI@MitoseLLC <shoko.kobayashi@gmail.com>
2025-10-03 17:24:20 +0900

    Update 4_4_bellman_ford.py

--- src/basics/4_4_bellman_ford.py
+++ src/basics/4_4_bellman_ford.py
@@ -1,28 +1,43 @@
 #coding : UTF-8
 
-def bellman_ford(vertices, edges, start) :
-    distances = {v: float('inf') for v in vertices}
-    predecessors = {v: None for v in vertices}
+class Bellmanford:
+    def __init__(self, vertices, edges, start):
+        self.vertices     = vertices
+        self.edges        = edges
+        self.start        = start
+        self.distances    = {v: float('inf') for v in vertices}
+        self.predecessors = {v: None for v in vertices}
 
-    distances[start] = 0
-    for i in range(len(vertices)-1):
-        for u, v, w in edges:
-            if distances[u] + w < distances[v]:
-                distances[v] = distances[u] + w
-                predecessors[v] = u
-        print(distances)
+    def update(self) :
+        self.distances[self.start] = 0
+        for i in range(len(self.vertices)-1):
+            for u, v, w in self.edges:
+                if self.distances[u] + w < self.distances[v]:
+                    self.distances[v] = self.distances[u] + w
+                    self.predecessors[v] = u
+            # print(self.distances)
 
-    return distances, predecessors   
+    def get_shortest_path(self, goal):
+        path = []
+        v = goal
+        while v is not None:
+            path.append(v)
+            # print(v, path)
+            v = self.predecessors[v]
+        path.reverse()
+        return path
 
-def get_shortest_path(predecessors, goal):
-    path = []
-    v = goal
-    while v is not None:
-        path.append(v)
-        # print(v, path)
-        v = predecessors[v]
-    path.reverse()
-    return path
+    def get_all_paths(self):
+        result = {}
+        for v in self.vertices:
+            result[v] = (self.distances[v], self.get_shortest_path(v))
+        return result
+    
+    def has_negative_cycle(self):
+        for u, v, w in self.edges:
+            if self.distances[u] + w < self.distances[v]:
+                return True
+        return False
 
 def main():
     start = "A"
@@ -32,13 +47,12 @@ def main():
         ("B", "C", -2),
         ("A", "C", 4)
     ]
-
-    distances, predecessors = bellman_ford(vertices, edges, start)
-    
-    print(distances["C"])
-    # A to C
-    path = get_shortest_path(predecessors, "C")
-    print(path)
+    bf = Bellmanford(vertices, edges, start)
+    bf.update()
+    result = bf.has_negative_cycle()
+    print("negative cycle" if result else "not negative cycle")
+    paths = bf.get_all_paths()
+    print(paths)
 
 if __name__ == "__main__":
     main()
